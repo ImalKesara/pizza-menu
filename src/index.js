55461bd4818ddac91c0b11217ba2c77bb6aff6eb
LoneWolf <imalkesara2001@gmail.com>
2024-03-03 18:16:30 +0530

    Finished my 1st react app :)

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ const pizzaData = [
     ingredients: "Tomato, mozarella, mushrooms, and onion",
     price: 12,
     photoName: "pizzas/funghi.jpg",
-    soldOut: false,
+    soldOut: true,
   },
   {
     name: "Pizza Salamino",
@@ -61,12 +61,19 @@ function App() {
 const Pizza = function ({ pizzaObj }) {
   // if (pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>{pizzaObj.soldOut ? "Sold out" : Number(pizzaObj.price)}</span>
+
+        {pizzaObj.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObj.price}</span>
+        )}
+
+        {/* <span>{pizzaObj.soldOut ? "Sold out" : Number(pizzaObj.price)}</span> */}
       </div>
     </li>
   );
